b6c70f364ee223b2e5246efa557cfec488174035
BerkOZOZ <106319216+BerkOZOZ@users.noreply.github.com>
2026-03-26 02:03:20 +0300

    Add files via upload

---
+++ Easy-Calculator.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int sayi1, sayi2;
+	int secim;
+	cout << "1.sayiyi giriniz: "; 
+	cin >> sayi1; 
+	cout << "2.sayiyi giriniz: ";
+	cin >> sayi2; 
+	cout << "1-Toplama\n2-cikartma\n3-Carpma\n4-bolme";
+	cin >> secim;
+	if (secim == 1)
+	cout << "Toplama secildi İki Sayinin Toplami:" << (sayi1 + sayi2);
+	
+	else if (secim == 2)
+	{
+		cout << "Cıkartma Secildi Sayi 1 Sayi 2 den su kadar buyuk:" << (sayi1 - sayi2);
+	}
+	else if (secim == 3)
+	{
+		cout << "Carpma Secildi:" << (sayi1 * sayi2);
+	}
+	else if (secim == 4)
+	{
+		cout << "Bölme Seçildi:" << (sayi1 / sayi2);
+	}
